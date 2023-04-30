056bdfb61566284b7d6e3c30796c6ba9ffd35745
hristoyanov <73996482+hristoyanov@users.noreply.github.com>
2023-04-30 14:10:28 +0300

    Added shortcode for single CPT Home view

--- wp-content/plugins/su-homes/shortcodes.php
+++ wp-content/plugins/su-homes/shortcodes.php
@@ -5,6 +5,7 @@
  *
  */
 function get_single_home( $atts = [], $content = null) {
+    global $post;
     $newATTS = shortcode_atts(
         [
         'id' => '',
@@ -12,26 +13,26 @@ function get_single_home( $atts = [], $content = null) {
         $atts
     );
     $post = get_post( $newATTS['id'] );
-    var_dump($post);
-    var_dump($post->post_content);
+    $post_meta = get_post_meta( $post->ID );
     ob_start(); ?>
-
- 
     <div class="property-card">
         <div class="property-primary">
-            <?php $visits = get_post_meta( $post->id, 'visits_count', true ); ?>
+            <?php $visits = $post_meta['visits_count'][0]; ?>
             <h2 class="property-title">
-                <a href="<?php $post->permalink; ?>"><?php $post->post_content; ?></a>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             </h2>
             <div class="property-meta">
-                <span class="meta-location">Ovcha Kupel, Sofia</span>
-                <span class="meta-total-area">Total area: 91.65 sq.m</span>
+                <span class="meta-location">Location: <?php echo get_the_terms( $post->ID, 'location' )[0]->name; ?></span>
+                <span class="meta-total-area">| Total area: <?php echo $post_meta['area'][0]; ?> sq.m</span>
+            </div>
+            <div>
+                <?php the_content(); ?>
             </div>
             <div class="property-details">
-                <span class="property-price">€ 100,815</span>
-                <span class="property-date">Posted on: <?php the_date(); ?></span>
+                <span class="property-price">Price: <?php echo $post_meta['price_currency'][0]; echo ' '; echo $post_meta['price'][0]; ?></span>
+                <span class="property-date">Posted on: <?php echo get_the_date(); ?></span>
                 <span class="property-date">| Views: <?php echo $visits; ?></span>
-                <span class="property-date">| Likes: <?php echo get_post_meta( get_the_ID(), 'likes', true ); ?></span>
+                <span class="property-date">| Likes: <?php echo $post_meta['likes'][0]; ?></span>
             </div>
         </div>
         <div class="property-image">
@@ -49,10 +50,9 @@ function get_single_home( $atts = [], $content = null) {
             </div>
         </div>
     </div>
-    <a id="<?php echo get_the_ID(); ?>" class="button like-button" href="#">Like</a>
+    <a id="<?php echo $post->ID; ?>" class="button like-button" href="#">Like</a>
 
-    <?php su_homes_update_home_views_count( get_the_ID() );
-
+    <?php su_homes_update_home_views_count( $post->ID );
     return ob_get_clean();
 }
 add_shortcode('get_single_home', 'get_single_home');
---
+++ wp-content/themes/softuni-homes/templates/template-shortcodes-page.php
@@ -0,0 +1,8 @@
+<?php
+/* Template Name: Shortcodes Page Template */
+?>
+
+<?php get_header();
+the_content();
+
+get_footer(); ?>
